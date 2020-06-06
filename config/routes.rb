809ca9266291dc7a77edbb5a4c967feb09a14b68
playa.tech <playa.tech.in@gmail.com>
2020-06-06 16:36:49 +0530

    Creating associations in rails. used scaffold generator....

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'  #This is to get the about page.  So we add to: controller: action. Then to pages controller
 end
